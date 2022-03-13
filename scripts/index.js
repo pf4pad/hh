9ae64c3b29304f6ed1906fee817ab3ba19a832de
Pavel Cherevach <pf4pad@gmail.com>
2022-03-13 15:13:39 +0200

    day2

--- scripts/index.js
+++ scripts/index.js
@@ -47,4 +47,111 @@ optionListPeriod.addEventListener('click', (e) => {
 
 // Выбор города
 
-const topCityBtn = document.querySelector('')
+const topCityBtn = document.querySelector('.top__city');
+const city = document.querySelector('.city');
+const cityClose = document.querySelector('.city__close');
+const cityRegionList = document.querySelector('.city__region-list');
+
+
+topCityBtn.addEventListener('click', () => {
+  city.classList.toggle('city_active')
+});
+
+cityRegionList.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.classList.contains('city__link')) {
+    topCityBtn.textContent = target.textContent;
+    city.classList.remove('city_active')
+  }
+});
+
+cityClose.addEventListener('click', () => {
+  city.classList.remove('city_active')
+})
+
+
+
+// Модальное окно
+
+const overlayVacancy = document.querySelector('.overlay_vacancy')
+const resultList = document.querySelector('.result__list')
+
+const modalClose = document.querySelector('.modal__close')
+
+resultList.addEventListener('click', (e) => {
+  const target = e.target
+  if (target.dataset.vacancy) {
+    e.preventDefault()
+    overlayVacancy.classList.add('overlay_active')
+  }
+});
+
+overlayVacancy.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target === overlayVacancy || target.classList.contains('modal__close'))
+    overlayVacancy.classList.remove('overlay_active')
+})
+
+// Вівод карточек 
+
+const createCard = (vacancy) => {
+  const { title,
+    id,
+    compensation,
+    workSchedule,
+    employer,
+    adress,
+    description,
+    date } = vacancy
+  const card = document.createElement('li')
+  card.classList.add('result__item');
+
+  card.insertAdjacentHTML('afterbegin', `<article class="vacancy">
+        <h2 class="vacancy__title">
+          <a class="vacancy__open-modal" href="#" data-vacancy="${id}">${title}</a>
+        </h2>
+        <p class="vacancy__compensation">${compensation}</p>
+        <p class="vacancy__work-schedule">${workSchedule}</p>
+        <div class="vacancy__employer">
+          <p class="vacancy__employer-title">${employer}</p>
+          <p class="vacancy__employer-address">${adress}</p>
+        </div>
+        <p class="vacancy__description">${description}</p>
+        <p class="vacancy__date">
+          <time datetime="2022-02-25">${date}</time>
+        </p>
+        <div class="vacancy__wrapper-btn">
+          <a class="vacancy__response vacancy__open-modal" href="#" data-vacancy="3515">Откликнуться</a>
+          <button class="vacancy__contacts">Показать контакты</button>
+        </div>
+  </article>
+`);
+  return card;
+};
+
+
+const renderCards = (data) => {
+
+  resultList.textContent = '';
+
+
+  const cards = data.map(createCard);
+  console.log(cards)
+  resultList.append(...cards)
+
+  // Через цикл
+  // for (let i = 0; i < data.length; i++) {
+  //   resultList.append(createCard(data[i]));
+  // }
+
+}
+
+const getData = () => fetch('http://localhost:3000/api/vacancy').then(response => response.json())
+
+const init = async () => {
+  const data = await getData();
+  renderCards(data)
+}
+
+init();
+renderCards(['1', '2', '3'])
